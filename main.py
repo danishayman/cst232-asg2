cc8f8af9a96d55407fbc83477b437d3f4dff18fa
a1man <danishaiman3b@gmail.com>
2024-01-01 19:28:39 +0800

    Move random_request generator
    Move random_request generator to ipynb

--- main.py
+++ main.py
@@ -10,17 +10,3 @@ head = 50
 direction = "left"
 random_request = [10, 20, 50, 100]
 
-for size in random_request:
-    # Generate 'size' random requests
-    arr = [random.randint(0, disk_size-1) for _ in range(size)]
-
-    print(f"\nRunning SCAN algorithm for {size} requests:")
-    SCAN(disk_size, arr, head, direction)
-
-    print(f"\nRunning C-SCAN algorithm for {size} requests:")
-    CSCAN(disk_size, arr, head)
-
-    print(f"\nRunning C-LOOK algorithm for {size} requests:")
-    CLOOK(arr, head)
-    
-    print("\n \n")
